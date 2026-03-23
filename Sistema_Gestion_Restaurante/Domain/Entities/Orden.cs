8428b79cf07f7a20fbaafcb89b103a72ee7d4246
AndresGu1 <agutierrez110316@umanizales.edu.co>
2026-03-22 19:09:05 -0500

    feat/code

---
+++ Sistema_Gestion_Restaurante/Domain/Entities/Orden.cs
@@ -0,0 +1,14 @@
+﻿using Sistema_Gestion_Restaurante.Domain.Common;
+
+using Sistema_Gestion_Restaurante.Domain.Common;
+
+namespace Sistema_Gestion_Restaurante.Domain.Entities
+{
+    public class Orden : EntityBase
+    {
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        public decimal Total { get; set; }
+        public string NumeroMesa { get; set; } = string.Empty;
+        public string Estado { get; set; } = "Pendiente";
+    }
+}
